c9c1c624b96673e70f8ed77b1ccf8a887aab7868
SyllabusUntold <ojikutuoladapo@gmail.com>
2022-10-03 04:17:01 +0000

    second time

--- Alphanumbers/settings.py
+++ Alphanumbers/settings.py
@@ -14,8 +14,6 @@ from email.charset import BASE64
 from lib2to3.pgen2.token import BACKQUOTE
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,7 +134,6 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-django_heroku.settings(locals())
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
